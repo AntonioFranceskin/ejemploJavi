5ceb9aa9841a7fba6c6c5eaaabf4903658cb38d2
Antonio Franceskin <afranceskin@dotlinked.es>
2019-06-19 19:25:39 +0200

    Agregar archivos de proyecto.

---
+++ ejemploJavi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ejemploJavi.MvcApplication" Language="C#" %>
---
+++ ejemploJavi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ejemploJavi.Startup))]
+namespace ejemploJavi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
